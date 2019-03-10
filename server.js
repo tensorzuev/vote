a5fcd51ee349a34d170c90f4012b9a8cc2bbbbd9
Dmitriy <xakkez@gmail.com>
2019-03-10 13:09:23 +0300

    super fast version for demonstration

---
+++ server.js
@@ -0,0 +1,93 @@
+const root = process.cwd(),
+   path = require('path'),
+   express = require('express'),
+   fs = require('fs'),
+   app = express(),
+   resourcesPath = path.join('', 'src');
+
+const global = (function() {
+   return this || (0, eval)('this');
+})();
+
+const clients = {};
+
+app.use(express.static(resourcesPath));
+const port = process.env.PORT || 777;
+var expressServer = app.listen(port);
+console.log('app available on port ' + port);
+
+// websockets
+
+const {createServer} = require('wss')
+
+global.currentInfo = {};
+
+var INTERVALS = [];
+let boss;
+
+const wss = createServer(function connectionListener(ws) {
+
+  var id = Math.random();
+  clients[id] = {
+    imboss: false,
+    answer: null,
+    ws: ws
+  };
+  console.log("новое соединение " + id);
+
+//    ws.send(JSON.stringify("2"));
+
+   ws.on('close', () => {
+      delete clients[id];
+   });
+
+   ws.on('message', (data) => {
+      handleMessage(JSON.parse(data), id);
+   });
+
+}).listen(8080, function() {
+   const {address, port} = this.address() // this is the http[s].Server
+   console.log('listening on http://%s:%d (%s)', /::/.test(address) ? '0.0.0.0' : address, port)
+});
+
+let activeAnswer;
+
+
+function handleMessage(data, id) {
+   switch (data.type) {
+      case "imboss":
+         clients[id].imboss = true;
+         boss = clients[id];
+         break;
+      case "newvote":
+
+        activeAnswer = {};
+        data.answers.forEach((el)=>{
+          activeAnswer[el] = 0;
+        });
+
+         for(let i in clients) {
+           if (clients.hasOwnProperty(i)) {
+             clients[i].answer = null;
+             try {
+               clients[i].ws.send(JSON.stringify({type: "newvote", answers: data.answers}));
+             } catch(e) {}
+           }
+         }
+         break;
+      case "answer":
+        clients[id].answer = data.answer;
+        activeAnswer[data.answer]++;
+        boss.ws.send(JSON.stringify({type: "newanswer", count: activeAnswer}));
+        break;
+      default:
+         return "wrong request";
+   }
+}
+
+
+function stopServers() {
+   // closeAllClientConnections(wss);
+   wss.close();
+   expressServer.close();
+}
