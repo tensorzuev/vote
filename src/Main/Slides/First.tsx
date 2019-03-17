961510b764606388b8dad8e82ee71fd6a46dfae8
Dmitriy <xakkez@gmail.com>
2019-03-17 23:01:23 +0300

    several slides, online voting

---
+++ src/Main/Slides/First.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export function First (props:HashMap<any>) {
+  return (
+    <div className='text-in-slide'>
+      <img src={props.logo} className='first-logo'/>
+      <h1>{props.title}</h1>
+
+      <div>{props.description}</div>
+
+      <div className='author-block'>{props.author}</div>
+      <div className='company-block'>{props.company}</div>
+    </div>
+  )
+}
